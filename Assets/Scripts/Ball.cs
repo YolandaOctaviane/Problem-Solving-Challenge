f1778fd55a34cebaa710b79016a7ec010c9d879b
yolanda octaviane <yolandaoctavi@gmail.com>
2021-10-10 14:49:27 +0700

    problem 3

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    private Rigidbody2D rigidBody2D;
+    public float speed;
+    public float currentSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody2D = GetComponent<Rigidbody2D>();
+        PushBall();
+    }
+
+    private void PushBall()
+    {
+        var xForce = Random.Range(-1f, 1f);
+        var yForce = Random.Range(-1f, 1f);
+        var force = new Vector2(xForce, yForce);
+        rigidBody2D.velocity = force.normalized * speed;
+    }
+
+    private void Update()
+    {
+        currentSpeed = rigidBody2D.velocity.magnitude;
+    }
+}
