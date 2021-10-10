248375497fb2f2f188994316383d6cc5e1cfb386
yolanda octaviane <yolandaoctavi@gmail.com>
2021-10-10 16:06:55 +0700

    problem 8

---
+++ Assets/Scripts/SquareSpawner.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SquareSpawner : MonoBehaviour
+{
+    public GameObject square;
+    public Ball player;
+    public int maxSquare;
+    public float maxX;
+    public float maxY;
+    public float ballArea;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = FindObjectOfType<Ball>();
+        var totalSquare = Random.Range(0,maxSquare);
+        for (int i = 0; i < totalSquare; i++)
+        {
+            Instantiate(square,positionInRange(),Quaternion.identity);
+        }
+    }
+
+    public Vector2 positionInRange()
+    {
+        var playerPositionX = player.transform.position.x;
+        var playerPositionY = player.transform.position.y;
+        Vector2 pos;
+        do
+        {
+            var xPosition = Random.Range(-maxX, maxX);
+            var yPosition = Random.Range(-maxY, maxY);
+            pos = new Vector2(xPosition, yPosition);
+        } while (pos.x >= -ballArea + playerPositionX && pos.x <= ballArea + playerPositionX
+            && pos.y >= -ballArea + playerPositionY && pos.y <= ballArea + playerPositionY);
+
+        return pos;
+    }
+
+}
